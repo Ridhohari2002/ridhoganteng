0a13064d506c017778dfc29db765a9c3d5c248e2
RidhoHari <113517286+Ridhohari2002@users.noreply.github.com>
2026-03-22 15:19:12 +0700

    Update Emote.lua

--- Emote.lua
+++ Emote.lua
@@ -7,6 +7,23 @@ return function(Window, State, Players, RunService)
     local EmoteTab = Window:Tab({ Title = "Emotes", Icon = "lucide:music" })
     local EmoteSection = EmoteTab:Section({ Title = "Emote Selection", Opened = true })
 
+    -- WAJIB DI ATAS
+    local EmoteData = {
+        ["24 Hour Cinderella"] = {anim = "137195203725366", sound = "121099446613414"},
+        ["Applause"] = {anim = "96328361165090", sound = "115490787020749"},
+        ["Arm Swing"] = {anim = "80552139463944", sound = "74216458932348"},
+        ["California Girls"] = {anim = "123552803041504", sound = "87899327891544"},
+        ["Ghoul"] = {anim = "130415594909401", sound = "123004139176580"},
+        ["Kyoufuu"] = {anim = "137322894494527", sound = "129064643026442"},
+        ["Mannrobics"] = {anim = "134677515695156", sound = "109596159930017"},
+        ["Quick Combo"] = {anim = "105592621576604", sound = "88505795419631"},
+        ["Schadenfreude"] = {anim = "138303785534052", sound = "92070710839040"},
+        ["Static"] = {anim = "95096724457263", sound = "70950516511572"},
+        ["Thriller"] = {anim = "99835792883875", sound = "139985043810748"},
+        ["Tor Monitor Ketua"] = {anim = "81792358514569", sound = "72665050838808"},
+        ["Wave"] = {anim = "99670106766588", sound = nil}
+    }
+
     local selectedEmote = nil
 
     local emoteNames = {}
