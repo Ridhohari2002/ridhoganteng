3b694f2c6005c06ccc797297cfa9d735fc15a682
RidhoHari <113517286+Ridhohari2002@users.noreply.github.com>
2026-03-24 15:40:22 +0700

    Update Visual.lua

--- Visual.lua
+++ Visual.lua
@@ -21,9 +21,7 @@ return function(Window, State, Players, RunService)
         Title = "Selected Entities",
         Multi = true,
         Values = {"Killer", "Survivor", "Generators", "Gates", "Pallets", "Windows", "Hooks"},
-        Callback = function(v) 
-            State.ESP.Selected = v 
-        end
+        Callback = function(v) State.ESP.Selected = v end
     })
     
     ESPSection:Toggle({
@@ -31,21 +29,10 @@ return function(Window, State, Players, RunService)
         Value = State.ESP.Enabled or false,
         Callback = function(v) 
             State.ESP.Enabled = v 
-            if not v then
-                -- Bersihkan semua saat OFF
-                for obj, _ in pairs(_G.ESP_Cache.Highlights) do
-                    if typeof(obj) == "Instance" or typeof(obj) == "table" then
-                        -- Fungsi pembersih global
-                        if _G.ESP_Cache.Highlights[obj] then _G.ESP_Cache.Highlights[obj]:Destroy() end
-                        if _G.ESP_Cache.Labels[obj] then _G.ESP_Cache.Labels[obj].Parent:Destroy() end
-                    end
-                end
-                table.clear(_G.ESP_Cache.Highlights)
-                table.clear(_G.ESP_Cache.Labels)
-            end
         end
     })
 
-    ESPSection:Toggle({ Title = "Show Names", Value = State.ESP.Names, Callback = function(v) State.ESP.Names = v end })
-    ESPSection:Toggle({ Title = "Show Distance", Value = State.ESP.Studs, Callback = function(v) State.ESP.Studs = v end })
+    ESPSection:Toggle({ Title = "Show Names", Value = false, Callback = function(v) State.ESP.Names = v end })
+    ESPSection:Toggle({ Title = "Show Distance", Value = false, Callback = function(v) State.ESP.Studs = v end })
+
 end
