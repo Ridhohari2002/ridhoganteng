3885a1ba16839c505f92969cc33d16e2bf8ec99a
RidhoHari <113517286+Ridhohari2002@users.noreply.github.com>
2026-03-25 21:16:51 +0700

    Create Aimbot2.lua

---
+++ Aimbot2.lua
@@ -0,0 +1,58 @@
+return function(Window, State, Players, RunService)
+    print("Mencoba memuat Tab Aimbot...")
+    local State = _G.SharedState
+
+--=====================================================
+-- AIMBOT
+--=====================================================
+local AimTab = Window:Tab({ 
+    Title = "Aimbot", 
+    Icon = "lucide:crosshair" 
+})
+
+local AimbotSettingSection = AimTab:Section({
+    Title = "Aimbot Setting",
+    Opened = true
+})
+
+-- Contoh kalau pakai Library Standar (sesuaikan dengan Library UI lu)
+AimbotSettingSection:CreateToggle({
+    Name = "🔦 Flashlight Aimbot",
+    CurrentValue = State.FlashlightAimbot, -- Pastikan State ini udah dibuat
+    Callback = function(Value)
+        State.FlashlightAimbot = Value
+        print("🔦 Flashlight Aimbot: ", Value)
+    end
+})
+
+AimbotSettingSection:Toggle({
+    Title = "Enable Crosshair",
+    Value = false,
+    Callback = function(v)
+        State.EnableCrosshair = v
+    end
+})
+AimbotSettingSection:Toggle({
+    Title = "Enable Toggle",
+    Desc = "On = Klik sekali | Off = Tahan tombol",
+    Value = false,
+    Callback = function(v)
+        State.AimbotToggleMode = v
+    end
+})
+AimbotSettingSection:Toggle({
+    Title = "Enable Through Wall",
+    Value = false,
+    Callback = function(v)
+        State.AimbotThroughWall = v
+    end
+})
+-- Set Keybind for user mobile on/off Aimbot Settings (Crosshair, Toggle, Through Wall)
+AimbotSettingSection:Keybind({
+    Title = "Set Keybind",
+    Default = Enum.KeyCode.E,
+    Callback = function(key)
+        State.AimbotKeybind = key
+    end
+})
+end
